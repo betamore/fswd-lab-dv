dc8e44debdf29b21f510e5ca43b3d5104ebc7e15
David Raynes <rayners@gmail.com>
2016-11-07 20:13:17 -0500

    Transitions, correct graph orientation

--- public/lib/main.js
+++ public/lib/main.js
@@ -13,7 +13,7 @@ var svg = d3.select("svg"),
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var xScale = d3.scaleBand().rangeRound([margin.left, width]).padding(0.1);
+var xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1);
 var yScale = d3.scalePow().rangeRound([ height, 0]).exponent(0.25);
 
 var xAxis = g.append("g")
@@ -29,7 +29,7 @@ function loadYear(year) {
   d3.csv('data/' + year + 'pres-data.csv', function(r) {
     r = _(r)
       .reject(['FEC ID', 'n/a'])
-      .filter(['STATE', 'Maryland'])
+      .filter(['STATE', 'Texas'])
       .map(function(d) {
         return {
           id: d['FEC ID'],
@@ -38,37 +38,39 @@ function loadYear(year) {
         };
       )
       .value();
-    console.log(r);
 
     yScale.domain([ 0, d3.max(r, function(d) { return d.votes; }) ]);
     xScale.domain(_.map(r, 'name'));
 
     xAxis.call(d3.axisBottom(xScale));
 
-    yAxis
-    .call(d3.axisLeft(yScale))
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Frequency");
+    yAxis.call(d3.axisLeft(yScale).ticks(10));
 
-    var sel = svg.selectAll('.bar')
-      .data(r, function(d) { return d.name; });
+    var sel = g.selectAll('.bar')
+      .data(r, function(d) { return d.id; });
     sel
       .enter()
       .append('rect')
       .attr('class', 'bar')
       .attr("x", function(d) { return xScale(d.name); })
+      .attr("y", height)
+      .attr("width", xScale.bandwidth())
+      .attr('height', 0)
+      .transition()
+      .attr("x", function(d) { return xScale(d.name); })
+      .attr("y", function(d) { return yScale(d.votes); })
+      .attr("height", function(d) { return height - yScale(d.votes); });
+
+    sel
+      .transition()
+      .attr("x", function(d) { return xScale(d.name); })
       .attr("y", function(d) { return yScale(d.votes); })
       .attr("width", xScale.bandwidth())
       .attr("height", function(d) { return height - yScale(d.votes); });
 
-    // sel.text(textForResult);
-    //
-    // sel
-    //   .exit()
-    //   .remove();
+    sel
+      .exit()
+      .transition()
+      .remove();
   });
 }
