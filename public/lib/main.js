6a4d4d6e547ba94baaec3da5d821218233c2c3bb
David Raynes <rayners@gmail.com>
2017-04-03 18:20:57 -0400

    Export d3 to window

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,6 +1,8 @@
 var d3 = require('d3');
 var _ = require('lodash');
 
+window.d3 = d3;
+
 d3.csv('data/2012pres-data.csv', function(r) {
 //   if (r["FEC ID"] == "n/a") { return undefined }
 //   return r;
