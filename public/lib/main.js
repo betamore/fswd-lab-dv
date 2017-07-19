d4c6cd8d8948fab7ab55a872ce6167b45984995f
David Raynes <rayners@gmail.com>
2017-07-19 15:25:00 -0400

    In class work

--- public/lib/main.js
+++ public/lib/main.js
@@ -14,26 +14,26 @@ window.d3 = d3;
 // console.log(d3.csv);
 
 var height = 400,
-    barHeight = 50;
+    width = 800;
 
 var y = d3.scaleLinear()
     .range([height, 0]);
 
 var x = d3.scaleBand()
-    .range([0, 800]);
+    .range([0, 800])
+    .padding(0.1);
+var chart = d3.select(".chart")
+    .attr("height", height)
+    .attr("width", width);
 
 d3.csv('data.csv', function(d) { d.number = +d.number; return d; }, function(data) {
     y.domain([0, d3.max(data, function(d) { return d.number; })]);
-    x.domain(data);
-
-    var chart = d3.select(".chart")
-        .attr("height", height)
-        .attr("width", barHeight * data.length);
+    x.domain(_.map(data, 'number'));
 
     var bar = chart.selectAll("g")
         .data(data)
       .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(" + i * barHeight + ", 0)"; });
+        .attr("transform", function(d, i) { return "translate(" + x(d.number) + ", 0)"; });
         // bar.append("text")
         //     .attr("x", barWidth / 2)
         //     .attr("y", function(d) { return y(d.value) + 3; })
@@ -47,7 +47,7 @@ d3.csv('data.csv', function(d) { d.number = +d.number; return d; }, function(dat
 
     bar.append("text")
         .attr("y", function(d) { return y(d.number) + 3; })
-        .attr("x", x.bandwidth())
+        .attr("x", x.bandwidth() / 2)
         .attr("dy", ".35em")
         .text(function(d) { return d.number; });
 });
