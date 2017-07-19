8cd725d1069ef436ba4fc4fc4b71bce794d4a25d
David Raynes <rayners@gmail.com>
2017-07-19 16:01:01 -0400

    systemjs+lite-server -> webpack

--- app/lib/main.js
+++ app/lib/main.js
@@ -1,3 +1,5 @@
+require('../main.css');
+
 var d3 = require('d3');
 var _ = require('lodash');
 
@@ -11,4 +13,3 @@ d3.csv('data/2012pres-data.csv', function(r) {
   r = _.reject(r, [ 'FEC ID', 'n/a']);
   console.log(r);
 });
-console.log(d3.csv);
